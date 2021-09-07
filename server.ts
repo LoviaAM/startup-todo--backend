0b7093be1842ff71c3a6d3eea1bdec60ea2715eb
Lovia AM <lsjmicah@gmail.com>
2021-09-07 15:54:45 +0100

    inclusion of http requests

--- server.ts
+++ server.ts
@@ -23,14 +23,58 @@ const app = express();
 app.use(express.json()); //add body parser to each following route handler
 app.use(cors()) //add CORS support to each following route handler
 
-const client = new Client(dbConfig);
+const client = new Client({
+  user: "academy", 
+  password: "",
+  host: "localhost",
+  port: 5432,
+  database : "checklist",
+}); // defines the client config 
 client.connect();
 
+// query for all values from the database
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
+  const dbres = await client.query('select * from checklist');
   res.json(dbres.rows);
 });
 
+// add user input 
+app.post("/input", async (req,res )=>{
+  try{
+    const {tag, description}= req.body
+    const newTodo = await client.query("INSERT INTO checklist (post_description, post_tag) VALUES ($1, $2)", [description, tag]
+    );
+    res.json(newTodo)
+  }catch (err){
+    console.log(err.message)
+  }
+});
+
+// allow  user to delete post 
+app.delete("post/:id", async (req,res)=> {
+  try{
+    const {id} = req.params;
+    const deleteTodo = await client.query(
+      "delete from checklist where post_id = $1",[id]
+    );
+    res.json("post deleted")
+  }catch (err){
+    console.log(err.message)
+  }
+})
+
+// edit the post
+app.put("/post/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const {description} = req.body;
+    const editTodo = await client.query(
+      "UPDATE checklist sert post_description = $1 where post_id = $2",[description, id]
+    )
+  } catch (err) {
+    console.log(err.message)
+  }
+})
 
 //Start the server on the given port
 const port = process.env.PORT;
