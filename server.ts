cf408f0803a3f9df7febeac019e64c364e3def13
Lovia AM <lsjmicah@gmail.com>
2021-09-08 14:08:40 +0100

    server requests complete

--- server.ts
+++ server.ts
@@ -11,8 +11,8 @@ config(); //Read .env file lines as though they were env vars.
 //For the ssl property of the DB connection config, use a value of...
 // false - when connecting to a local DB
 // { rejectUnauthorized: false } - when connecting to a heroku DB
-const herokuSSLSetting = { rejectUnauthorized: false }
-const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
+const herokuSSLSetting = { rejectUnauthorized: false };
+const sslSetting = process.env.LOCAL ? false : herokuSSLSetting;
 const dbConfig = {
   connectionString: process.env.DATABASE_URL,
   ssl: sslSetting,
@@ -21,65 +21,81 @@ const dbConfig = {
 const app = express();
 
 app.use(express.json()); //add body parser to each following route handler
-app.use(cors()) //add CORS support to each following route handler
+app.use(cors()); //add CORS support to each following route handler
 
 const client = new Client({
-  user: "academy", 
+  user: "academy",
   password: "",
   host: "localhost",
   port: 5432,
-  database : "checklist",
-}); // defines the client config 
+  database: "checklist",
+}); // defines the client config
 client.connect();
 
 // query for all values from the database
-app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from checklist');
+app.get("/start-up", async (req, res) => {
+  const dbres = await client.query("select * from checklist");
   res.json(dbres.rows);
 });
 
-// add user input 
-app.post("/input", async (req,res )=>{
-  try{
-    const {tag, description}= req.body
-    const newTodo = await client.query("INSERT INTO checklist (post_description, post_tag) VALUES ($1, $2)", [description, tag]
+// user should be able to see all posts added to the todo list
+app.get("/start-up/viewpost", async (req, res) => {
+  try {
+    const allPosts = await client.query(
+      "Select * from checklist ORDER by post_tag DESC"
     );
-    res.json(newTodo)
-  }catch (err){
-    console.log(err.message)
+    res.json(allPosts.rows);
+  } catch (err) {
+    console.log(err.message);
   }
 });
 
-// allow  user to delete post 
-app.delete("post/:id", async (req,res)=> {
-  try{
-    const {id} = req.params;
-    const deleteTodo = await client.query(
-      "delete from checklist where post_id = $1",[id]
+// add user input
+app.post("/start-up/input", async (req, res) => {
+  try {
+    const { tag, description } = req.body;
+    const newTodo = await client.query(
+      "INSERT INTO checklist (post_description, post_tag) VALUES ($1, $2)",
+      [description, tag]
     );
-    res.json("post deleted")
-  }catch (err){
-    console.log(err.message)
+    res.json(newTodo);
+  } catch (err) {
+    console.log(err.message);
   }
-})
+});
+
+// allow  user to delete post
+app.delete("/start-up/post/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await client.query(
+      "delete from checklist where post_id = $1",
+      [id]
+    );
+    res.json("post deleted");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 // edit the post
-app.put("/post/:id", async (req, res) => {
+app.put("/start-up/post/:id", async (req, res) => {
   try {
-    const {id} = req.params;
-    const {description} = req.body;
+    const { id } = req.params;
+    const { description } = req.body;
     const editTodo = await client.query(
-      "UPDATE checklist sert post_description = $1 where post_id = $2",[description, id]
-    )
+      "UPDATE checklist sert post_description = $1 where post_id = $2",
+      [description, id]
+    );
   } catch (err) {
-    console.log(err.message)
+    console.log(err.message);
   }
-})
+});
 
 //Start the server on the given port
 const port = process.env.PORT;
 if (!port) {
-  throw 'Missing PORT environment variable.  Set it in .env file.';
+  throw "Missing PORT environment variable.  Set it in .env file.";
 }
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
